82816b7e44048c8ea027ef112c61f4939b0fe93c
Isuru Kaushika <kawshikaisuru@gmail.com>
2025-05-27 14:30:03 +0530

    FullAdder

--- 10-FulllAdder/fulladder1b.v
+++ 10-FulllAdder/fulladder1b.v
@@ -10,46 +10,7 @@ assign {cout,s}=a+b+cin;
 
 endmodule
 
-module comp2bfull(
-    input wire[1:0] a,
-    input wire[1:0] b,
-    output reg a_eq_b,
-    output reg a_gt_b,
-    output reg a_lt_b
-);
 
-always @(*) begin
-    if(a==b)
-    a_eq_b = (a == b);
-    a_gt_b = (a > b);
-    a_lt_b = (a < b);
-end
-
-
-module busz(
-    input wire [3:0] a,
-    input wire [3:0] b,
-    input wire c,
-    output wire [3:0] p,
-)
-
-wire [3:0] cbus;
-
-assign cbus ={4{c}};
-
-assign p =( a&(~cbus)|
-           b&cbus);
-endmodule
-
-module simplemux(
-    input wire [3:0] a,
-    input wire [3:0] b,
-    input wire sel,
-    output wire [3:0] c,
-);
-
-assign c = sel? a:b;
-endmodule
 
 
 
